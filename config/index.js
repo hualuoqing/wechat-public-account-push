8294b321591a30e3e82aea6a0c7611db04be2472
hualuoqing <55091389+hualuoqing@users.noreply.github.com>
2022-09-17 19:27:28 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,17 +64,17 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "o5o9U6B8b_KhgI71JWuFWhdiw_IQ",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "TeCJ2dUuOXtX4qPlDzAYRWpXmZ9QQqnP-0MFIUVqsGA",
+      useTemplateId: "jeDggKBz__toEM4fHmCuIGVJxu5f6h_CvX1aTd8yyQI",
       // 所在省份
       province: "湖北",
       // 所在城市
       city: "武汉",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '06-25',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "www.已经是世界尽头啦！.(╯▔皿▔)╯",
+      openUrl: "www.已经是世界尽头啦！.(╯▔皿▔)╯.com",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
