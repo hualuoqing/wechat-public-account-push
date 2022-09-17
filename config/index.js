ac63008c2542922e101feb3b15c75569697f0685
hualuoqing <55091389+hualuoqing@users.noreply.github.com>
2022-09-17 20:46:54 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "o5o9U6B8b_KhgI71JWuFWhdiw_IQ",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "73Sk8brzTemo1QeIEPVjhzWfi7GXOyBs90_-z-GDC2Q",
+      useTemplateId: "poSVUc_TXlk-FDb-K1IrLZdiJDdQUgC31yXTHhurfK4",
       // 所在省份
       province: "湖北",
       // 所在城市
