d6320216ddda48ef8b03f133fb1001d5ee078442
hualuoqing <55091389+hualuoqing@users.noreply.github.com>
2022-09-17 21:02:08 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -78,17 +78,17 @@ export const config = {
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "萌妹", "year": "2001", "date": "06-25"},
+        {"type": "*生日", "name": "萌妹", "year": "2001", "date": "04-16"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        {"keyword": "love_day", date: "2001-06-25"},
+        {"keyword": "love_day", date: "2001-04-16"},
         // 结婚纪念日
-        {"keyword": "marry_day", date: "2022-09-09"},
+        {"keyword": "marry_day", date: "2021-01-07"},
         // 退伍日
-        {"keyword": "ex_day", date: "2022-09-10"},
+        {"keyword": "ex_day", date: "2022-09-09"},
       ]
     },
     {
